49ad19cd44aad9c17f6a4c72b28cf9af78be3bf3
gtech29 <juan.rodriguez.517@my.csun.edu>
2024-06-13 13:45:26 -0700

    update

--- CsharpProjects/DataTypes/Exercises/CombineStrings/Program.cs
+++ CsharpProjects/DataTypes/Exercises/CombineStrings/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+double total = 0;
+string message = "";
+
+
+foreach (string value in values)
+{
+  if (double.TryParse(value, out double number))
+  {
+    total += number;
+  }
+  else
+  {
+    message += value;
+  }
+
+
+}
+Console.WriteLine($"The total is: {total}");
+Console.WriteLine($"Message: {message}");
